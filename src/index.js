371d6918c683978099ef5235a7441cfa690bbcf4
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-07-14 19:18:45 -0400

    Fix Linter 1

--- src/index.js
+++ src/index.js
@@ -1,91 +1,91 @@
 import _ from 'lodash';
 
-const erase =document.querySelector('.erase-container');
+const erase = document.querySelector('.erase-container');
 function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
+  const element = document.createElement('div');
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+const todo = [
+  {
+    description: 'Complete to-do-List',
+    completed: false,
+    id: 0,
+    index: 0,
+  },
+  {
+    description: 'Clean Washes',
+    completed: true,
+    id: 1,
+    index: 1,
+  },
+  {
+    description: 'Do more examples',
+    completed: true,
+    id: 2,
+    index: 2,
+  },
+];
+
+class Todo {
+  constructor() {
+    this.todo = null;
   }
-
-  const todo = [
-    {
-      description: 'Complete to-do-List',
-      completed: false,
-      id: 0,
-      index:0,
-    },
-    {
-      description: 'Clean Washes',
-      completed: true,
-      id: 1,
-      index: 1,
-    },
-    {
-      description: 'Do more examples',
-      completed: true,
-      id: 2,
-      index: 2,
-    },
-  ];
-
-  class Todo{
-    constructor(){
-      this.todo = null;
-    }
-    setTodo(todolist){
-      this.todo =todolist;
-    }
-    getTodo(){
+  setTodo(todolist) {
+    this.todo = todolist;
+  }
+  getTodo() {
     return this.todo;
-    }
-    showall(){
-      for(let i = 0; i<this.todo.length; i += 1){
-        const activity =this.todo[i];
-        const container =document.createElement('li');
-        container.classList.add('tdl-element');
+  }
+  showall() {
+    for (let i = 0; i < this.todo.length; i += 1) {
+      const activity = this.todo[i];
+      const container = document.createElement('li');
+      container.classList.add('tdl-element');
 
-        const statusC = document.createElement('div');
-        statusC.classList.add('tdle-status-c');
+      const statusC = document.createElement('div');
+      statusC.classList.add('tdle-status-c');
 
-        const status = document.createElement('i');
-        status.classList.add('far');
-        if (activity.completed===true){
-          status.classList.add('fa-check-square');
-        } else {
-          status.classList.add('fa-square');
-          }
-          statusC.appendChild(status);
-
-          container.appendChild(statusC);
-
-          const text = document.createElement('div');
-          text.classList.add('tdle-text-c');
-          text.textContent = activity.description;
-
-          container.appendChild(text);
-
-          const lastC = document.createElement('div');
-          lastC.classList.add('tdle-last-c');
-          
-          const last = document.createElement('i');
-          last.classList.add('fas');
-          last.classList.add('fa-ellipsis-v');
-
-          lastC.appendChild(last);
-
-          container.appendChild(lastC);
-          console.log("fiesta")
-          erase.parentNode.insertBefore(container, erase);
-
+      const status = document.createElement('i');
+      status.classList.add('far');
+      if (activity.completed === true) {
+        status.classList.add('fa-check-square');
+      } else {
+        status.classList.add('fa-square');
       }
+      statusC.appendChild(status);
+
+      container.appendChild(statusC);
+
+      const text = document.createElement('div');
+      text.classList.add('tdle-text-c');
+      text.textContent = activity.description;
+
+      container.appendChild(text);
+
+      const lastC = document.createElement('div');
+      lastC.classList.add('tdle-last-c');
+
+      const last = document.createElement('i');
+      last.classList.add('fas');
+      last.classList.add('fa-ellipsis-v');
+
+      lastC.appendChild(last);
+
+      container.appendChild(lastC);
+      console.log("fiesta")
+      erase.parentNode.insertBefore(container, erase);
+
     }
   }
-  let list = new Todo();
-  list.setTodo(todo);
-  console.log(list);
-  list.showall();
+}
+let list = new Todo();
+list.setTodo(todo);
+console.log(list);
+list.showall();
 
-  import './style.css';
+import './style.css';
