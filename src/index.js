8e07d36c428ec22df1bed219870c07442e56be84
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-07-14 15:30:11 -0400

    added css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 
+const ()
 function component() {
     const element = document.createElement('div');
   
@@ -9,5 +10,37 @@ function component() {
     return element;
   }
 
+  todo = [
+    {
+      description: 'Complete to-do-List',
+      completed: 'false',
+      id: 0,
+      index:0,
+    },
+    {
+      description: 'Clean Washes',
+      completed: 'true',
+      id: 1,
+      index: 1,
+    },
+    {
+      description: 'Do more examples',
+      completed: 'true',
+      id: 2,
+      index: 2,
+    },
+  ];
 
+  class Todo{
+    constructor(){
+      this.todo = null;
+    }
+    setTodo(todolist){
+      this.todo =todolist;
+    }
+    getTodo(){
+    return this.todo;
+    }
+    
+  }
   import './style.css';
