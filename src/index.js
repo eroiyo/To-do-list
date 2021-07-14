1f7f43345050b6a5f8efeb075153b82aa2afc532
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-07-14 18:57:17 -0400

    Display Function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const ()
+const erase =document.querySelector('.erase-container');
 function component() {
     const element = document.createElement('div');
   
@@ -10,22 +10,22 @@ function component() {
     return element;
   }
 
-  todo = [
+  const todo = [
     {
       description: 'Complete to-do-List',
-      completed: 'false',
+      completed: false,
       id: 0,
       index:0,
     },
     {
       description: 'Clean Washes',
-      completed: 'true',
+      completed: true,
       id: 1,
       index: 1,
     },
     {
       description: 'Do more examples',
-      completed: 'true',
+      completed: true,
       id: 2,
       index: 2,
     },
@@ -41,6 +41,51 @@ function component() {
     getTodo(){
     return this.todo;
     }
-    
+    showall(){
+      for(let i = 0; i<this.todo.length; i += 1){
+        const activity =this.todo[i];
+        const container =document.createElement('li');
+        container.classList.add('tdl-element');
+
+        const statusC = document.createElement('div');
+        statusC.classList.add('tdle-status-c');
+
+        const status = document.createElement('i');
+        status.classList.add('far');
+        if (activity.completed===true){
+          status.classList.add('fa-check-square');
+        } else {
+          status.classList.add('fa-square');
+          }
+          statusC.appendChild(status);
+
+          container.appendChild(statusC);
+
+          const text = document.createElement('div');
+          text.classList.add('tdle-text-c');
+          text.textContent = activity.description;
+
+          container.appendChild(text);
+
+          const lastC = document.createElement('div');
+          lastC.classList.add('tdle-last-c');
+          
+          const last = document.createElement('i');
+          last.classList.add('fas');
+          last.classList.add('fa-ellipsis-v');
+
+          lastC.appendChild(last);
+
+          container.appendChild(lastC);
+          console.log("fiesta")
+          erase.parentNode.insertBefore(container, erase);
+
+      }
+    }
   }
+  let list = new Todo();
+  list.setTodo(todo);
+  console.log(list);
+  list.showall();
+
   import './style.css';
